284297b437542ae57d0bf5b45e0fb266a59fc3d0
Ale190107 <alejandrolopez190107@gmail.com>
2022-12-22 20:58:52 -0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+var password = document.getElementById('EnterPassword');
+var toggler = document.getElementById('toggler');
+
+showHidePassword = () => {
+  if (password.type == 'password') {
+
+    password.setAttribute('type', 'text');
+    toggler.classList.add('fa-eye-slash');
+
+  } else {
+
+    toggler.classList.remove('fa-eye-slash');
+    password.setAttribute('type', 'password');
+
+  }
+};
+toggler.addEventListener('click', showHidePassword);
